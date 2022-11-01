ac5608cd35cf50445a2437dfe8c247a1133ee205
Abraão Oliveira <113057132+abraao0liveira@users.noreply.github.com>
2022-11-01 10:59:12 -0300

    Add Fibonacci

--- Fibonacci/Fibonacci/fibonacci.cs
+++ Fibonacci/Fibonacci/fibonacci.cs
@@ -4,6 +4,24 @@ public class fibonacci
 {
     static void Main()
     {
-        
+        Console.Clear();
+        Console.WriteLine("Digite a quantidade de números a serem imprimidos: ");
+        int total = int.Parse(Console.ReadLine());
+        Console.Clear();
+        Fibonacci(1, total, 0, 1);
+    }
+    static int Fibonacci(int number, int total, int aux, int i)
+    {
+        if (i > total)
+        {
+            return number;
+        }
+        Console.WriteLine(number);
+
+        var result = number + aux;
+        aux = number;
+        number = result;
+        i++;
+        return Fibonacci(result, total, aux, i);
     }
 }
